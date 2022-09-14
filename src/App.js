32fe15dec9a4feb6f86189033fc32638ee8c92d4
sowmya2921 <sowmyadeepu2921@gmail.com>
2022-09-14 18:42:49 +0530

    navigation with button click

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Countries from './Countries';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 
 function App() {
+  var navigate = useNavigate();
+  function abc(){
+    navigate("/home")
+  }
   
   return (
     <div className='border m-2 p-2'>
       <Link to="/home">Home</Link>&nbsp;&nbsp;&nbsp;
       <Link to="/countries">Countries</Link>&nbsp;&nbsp;&nbsp;
+      <button onClick={abc}>click here to goto home</button>
       <Outlet></Outlet>
     </div>
   );
